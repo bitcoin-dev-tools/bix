752147c2eac55461a6bfddb4ad18172dbf04ab54
will <will@256k1.dev>
2025-05-20 12:33:24 +0100

    support more platforms

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Bitcoin development environment";
+  description = "Bitcoin development environment with tools for building, testing, and debugging";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -11,87 +11,83 @@
     flake-utils,
     ...
   }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-darwin"] (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-        isLinux = pkgs.stdenv.isLinux;
-        lib = pkgs.lib;
-      in {
-        formatter = pkgs.alejandra;
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      isLinux = pkgs.stdenv.isLinux;
+      lib = pkgs.lib;
+    in {
+      formatter = pkgs.alejandra;
 
-        devShells.default = pkgs.mkShell {
-          CMAKE_GENERATOR = "Ninja";
-          LDFLAGS = "-fuse-ld=mold";
-          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.gcc14.cc pkgs.capnproto];
-          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-          QT_PLUGIN_PATH = "${pkgs.qt6.qtbase}/${pkgs.qt6.qtbase.qtPluginPrefix}";
+      devShells.default = pkgs.mkShell {
+        CMAKE_GENERATOR = "Ninja";
+        LDFLAGS = "-fuse-ld=mold";
+        LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.gcc14.cc pkgs.capnproto];
+        LOCALE_ARCHIVE = lib.optionalString isLinux "${pkgs.glibcLocales}/lib/locale/locale-archive";
+        QT_PLUGIN_PATH = "${pkgs.qt6.qtbase}/${pkgs.qt6.qtbase.qtPluginPrefix}";
 
-          nativeBuildInputs = with pkgs;
-            [
-              byacc
-              ccache
-              cmake
-              gcc14
-              gnum4
-              gnumake
-              include-what-you-use
-              llvmPackages_20.clang
-              llvmPackages_20.clang-tools
-              llvmPackages_20.stdenv
-              mold-wrapped
-              ninja
-              pkg-config
-              qt6.wrapQtAppsHook
-            ]
-            ++ (
-              if isLinux
-              then [
-                libsystemtap
-                linuxPackages.bcc
-                linuxPackages.bpftrace
-              ]
-              else []
-            );
+        nativeBuildInputs = with pkgs;
+          [
+            autoconf
+            automake
+            byacc
+            ccache
+            cmake
+            gcc14
+            gnum4
+            gnumake
+            include-what-you-use
+            libtool
+            llvmPackages_20.clang
+            llvmPackages_20.clang-tools
+            llvmPackages_20.stdenv
+            mold-wrapped
+            ninja
+            pkg-config
+            qt6.wrapQtAppsHook
+          ]
+          ++ lib.optionals isLinux [
+            libsystemtap
+            linuxPackages.bcc
+            linuxPackages.bpftrace
+          ];
 
-          buildInputs = with pkgs;
-            [
-              boost
-              capnproto
-              codespell
-              db4
-              gdb
-              hexdump
-              libevent
-              python312Packages.flake8
-              python312Packages.lief
-              python312Packages.mypy
-              python312Packages.pyzmq
-              python312Packages.vulture
-              qrencode
-              qt6.qtbase
-              qt6.qttools
-              sqlite
-              uv
-              valgrind
-              zeromq
-            ]
-            ++ (
-              if isLinux
-              then [
-                libsystemtap
-                linuxPackages.bcc
-                linuxPackages.bpftrace
-                python312Packages.bcc
-              ]
-              else []
-            );
+        buildInputs = with pkgs;
+          [
+            boost
+            capnproto
+            codespell
+            db4
+            gdb
+            hexdump
+            libevent
+            python312Packages.flake8
+            python312Packages.lief
+            python312Packages.mypy
+            python312Packages.pyzmq
+            python312Packages.vulture
+            qrencode
+            qt6.qtbase
+            qt6.qttools
+            sqlite
+            uv
+            valgrind
+            zeromq
+          ]
+          ++ lib.optionals isLinux [
+            libsystemtap
+            linuxPackages.bcc
+            linuxPackages.bpftrace
+            python312Packages.bcc
+          ]
+          ++ lib.optionals (system == "aarch64-darwin") [
+            pkgs.darwin.apple_sdk.frameworks.CoreServices
+          ];
 
-          shellHook = ''
-            # Opinionated defaults
-            export CC=clang
-            export CXX=clang++
-          '';
-        };
-      }
-    );
+        shellHook = ''
+          export CC=clang
+          export CXX=clang++
+          export PATH=$PATH:${pkgs.ccache}/bin
+        '';
+      };
+    });
 }
